9c6ceaebc8a087cc0d32c4787e5543dad03fcb94
Farid ullah <faridullah996@gmail.com>
2022-11-03 22:22:47 +0500

    flowy initialized with basic drag and drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
+
+declare var flowy: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'flowy-demo';
+export class AppComponent implements AfterViewInit {
+	spacing_x = 40;
+	spacing_y = 100;
+
+	constructor() { }
+
+	ngAfterViewInit(): void {
+		const canvas = document.getElementById("canvas");
+		flowy(canvas, this.onGrab, this.onRelease, this.onSnap, this.onRearrange, this.spacing_x, this.spacing_y);		
+	}
+
+	onGrab(block: any) {
+		console.log('On Grab .....', block);
+	}
+
+	onRelease() {
+		console.log('On Release .....');
+	}
+
+	onSnap(block: any, first: any, parent: any) {
+		console.log('On Snap ....', block, first, parent);
+		return true;
+	}
+
+	onRearrange(block: any, parent: any){
+		console.log('On Rearrange ....', block, parent);
+		return true;
+	}
 }
